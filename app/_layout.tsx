78a875c36c59e057baf20e4a9461272e44732883
rayen <challoufrayen09@gmail.com>
2026-04-14 10:33:16 +0100

    added home page , Qr code,profile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,50 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: "#0A66C2" },
+        headerTintColor: "white",
+        tabBarActiveTintColor: "#0A66C2",
+      }}
+    >
+      {/* HOME */}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Accueil",
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      {/* QR CODE */}
+      <Tabs.Screen
+        name="qr"
+        options={{
+          title: "QR Code",
+          tabBarLabel: "QR",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="qr-code-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      {/* PROFILE */}
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profil",
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
